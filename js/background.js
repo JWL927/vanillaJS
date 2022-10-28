44d8eaabd2131660cf1d00e51a240f7900938ec3
Musix <ljw927@naver.com>
2022-10-28 10:09:34 +0900

    add logo, container height auto, delete bgimages

--- js/background.js
+++ js/background.js
@@ -1,7 +1,13 @@
-const images = ["3.jpg", "4.jpg", "6.jpg"];
+const images = ["1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
+const logoText = document.querySelector("#element");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.className = "bgImg";
+
+if(chosenImage === "2.jpg") {
+    logoText.style.color = "white";
+}
 
 document.body.appendChild(bgImage);
